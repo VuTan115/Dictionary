630bb219af0a51327d4e6bd96dd7ca30ec4f7509
unknown <dangthanhhoa2001@gmail.com>
2020-09-30 16:20:41 +0700

    hi:

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+}
