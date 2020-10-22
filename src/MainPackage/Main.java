68a0408c59c29a6ef44c6ac997a3e352e1ae6d11
Vũ Cao Tân <60535960+VuTan115@users.noreply.github.com>
2020-10-22 15:05:32 +0700

    1 don hon don

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/MainStage.fxml"));
         primaryStage.setTitle("Dictionary");
         //primaryStage.getIcons().add(new Image("ASset/book.png"));
-        primaryStage.getIcons().add(new Image("https://www.flaticon.com/svg/static/icons/svg/3528/3528186.svg"));
+        //primaryStage.getIcons().add(new Image("https://www.flaticon.com/svg/static/icons/svg/3528/3528186.svg"));
         primaryStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
     }
